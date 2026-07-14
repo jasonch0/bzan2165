ce9a96c8935723e1a4bcd7aa48388971dd4549e9
Jason Cho <jcho3589@gmail.com>
2026-07-14 13:37:44 -0400

    1.3

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = "I am a programmer!"
-    // makes it mutable, private makes it only accessible in the struct 
+    // makes it mutable, private makes it only accessible in the struct
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me") {
-                message = "Awesome"
-                print(message)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            Spacer()
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                Button("Great!") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
