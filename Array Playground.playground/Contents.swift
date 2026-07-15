6240efcdf2be0361d9628a706dd7be2cb69d9977
Jason Cho <jcho3589@gmail.com>
2026-07-15 14:41:45 -0400

    1.9

---
+++ Array Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var messages = ["You Are Awesome!", "You are great!"]
+print(messages)
+print(messages[0])
+messages.remove(at: 0)
+
+print("There are \(messages.count) elements in the array")
+
+
+// Empty Arrays
+var grades: [Int] = []
+// var grades = [Double]()
+
+
--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -9,23 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var num = 0
+    @State private var image = ""
+    @State private var message = ""
         // makes it mutable, private makes it only accessible in the struct
     var body: some View {
         VStack {
             Spacer()
             
-            Image("image\(num)")
+            
+            Image(image)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+            
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
+                let messages = ["Hi",
+                                "Bill",
+                                "foo",
+                                "When the Genius Bar Needs Help, They Call You!"]
+                message = messages[num]
+                image = "image\(num)"
+                
                 num += 1
                 
-                if num > 9 {
+                if num == messages.count  {
                     num = 0
                 }
             }
