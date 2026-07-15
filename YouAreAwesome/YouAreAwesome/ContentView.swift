934a811863082517db0b3f0d48a8fa9751f35be8
Jason Cho <jcho3589@gmail.com>
2026-07-15 13:39:40 -0400

    1.8

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -8,34 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image = ""
-    @State private var msg = ""
-    // makes it mutable, private makes it only accessible in the struct
+    @State private var num = 0
+        // makes it mutable, private makes it only accessible in the struct
     var body: some View {
         VStack {
             Spacer()
             
-            Image(image)
+            Image("image\(num)")
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
             
-            Text(msg)
-                .font(.largeTitle)
-                .foregroundStyle(.red)
-                .fontWeight(.heavy)
-
             Spacer()
             
             Button("Press Me!") {
-                let message1 = "You Are Awesome!"
-                let img1 = "image0"
-                let message2 = "You Are Great!"
-                let img2 = "image1"
-
-                msg = msg == message1 ?  message2 : message1
-                image = image == img1 ? img2 : img1
+                num += 1
+                
+                if num > 9 {
+                    num = 0
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
